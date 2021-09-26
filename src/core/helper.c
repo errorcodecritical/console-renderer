1b502e70883c09ecbd0c0841cbb3a0e6a3d79803
CodeCritical <80753888+Code-Critical@users.noreply.github.com>
2021-09-26 13:18:55 +0100

    Delete src directory

--- src/core/helper.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "helper.h"
-
-void msleep(int millisec) {
-    struct timespec req = {0};
-    req.tv_sec = 0;
-    req.tv_nsec = millisec * 1000000L;
-    nanosleep(&req, (struct timespec*)NULL);
-}
--- src/core/helper.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HELPER_H
-#define __HELPER_H
-
-#include <time.h>
-
-extern void msleep(int millisec);
-
-#endif
